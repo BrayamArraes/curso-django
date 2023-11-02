fa8d03c083d7ee8322a46f231cee8155aa833382
Brayam Arraes <brayam.arraes2@gmail.com>
2023-11-02 11:36:37 -0300

    created banco de dados / alterando urls

--- receitas/urls.py
+++ receitas/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from receitas import views
 
+app_name = 'receitas'
+
 
 urlpatterns = [
-    path('', views.home),                     # home
-    path('receitas/<int:id>/', views.receitas),
+    path('', views.home, name='home'),                     # home
+    path('receitas/<int:id>/', views.receitas, name='receitas'),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-
+from utils.receitas.factory import make_receitas
 
 def home(request):
     return render(request, 'receitas/pages/home.html', context={
-        'nome': 'Brayam Arraes !',
+        'receitas': [make_receitas () for _ in range(10)],
     })
 
 
 def receitas(request, id):
     return render(request, 'receitas/pages/receita-view.html', context={
-        'nome': 'Brayam Arraes !',
+        'receita': make_receitas(),
+        'is_detalhe_page': True
     })
