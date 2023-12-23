de7375eb4cb95d37102b17835a27e052aa6cac3c
Brayam Arraes <brayam.arraes2@gmail.com>
2023-12-23 20:13:29 -0300

    adicionando paginaçao/ e linkando na home

--- receitas/views.py
+++ receitas/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render, get_list_or_404
-from utils.receitas.factory import make_receitas
+from django.shortcuts import render
 from receitas.models import receita
 from django.http.response import Http404
+from django.db.models import Q
+from django.core.paginator import Paginator
+from utils.pagination import make_pagination_range
 
 
 def home(request):
     receitas = receita.objects.filter(is_published=True).order_by('-id')
+
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(receitas, 9)
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+        )
+
     return render(request, 'receitas/pages/home.html', context={
-        'receitas': receitas,
+         'receitas': page_obj,
+         'pagination_range': pagination_range,
     })
 
 
@@ -37,11 +55,21 @@ def search(request):
 
     if not search_term:
         raise Http404()
-     
+    
+    # Q significa trocar o AND 'E' para o OR 'OU' tendo que importar from django.db.models import Q 
+    # order.by('-id') significa ordernar os ids na ordem decrescente
+    receitas = receita.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
      
     return render(request, 'receitas/pages/search.html', {
         'page_title': f'Pesquisa por "{search_term}"',
         'search_term': search_term,
+        'receitas': receitas,
         })
       
 
